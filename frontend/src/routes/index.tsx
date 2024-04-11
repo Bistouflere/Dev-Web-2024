d79c2cb85eec3f7f9d6af38a0818c39a0475e5c4
Simon-Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2024-04-11 16:12:37 +0200

    Update index.tsx

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -31,66 +31,73 @@ export default function IndexPage() {
           tournaments with ease.
         </Balancer>
       </section>
-      <div>
+      <section>
         {tournaments ? (
-          <section className="flex items-center justify-center">
-            <Carousel className="w-full">
-              <CarouselContent>
-                {tournaments.map((response) => (
-                  <CarouselItem className="basis-1/3">
-                    <Card className="w-full">
-                      <CardContent>
-                        <div className="flex flex-1 mt-4 gap-2 flex-col justify-between lg:flex-row">
-                          <div>
-                            <CardTitle>{response.name}</CardTitle>
-                            <CardDescription>
-                              {response.description || "No description"}
-                            </CardDescription>{" "}
-                          </div>
-                          <Link
-                            to={`/tournaments/${response.id}`}
-                            className="items-center w-1/2"
-                          >
-                            <img
-                              src={response.image_url || undefined}
-                              alt={response.name}
-                              className="rounded-lg"
-                            />
-                          </Link>
+          <Carousel
+            className="w-full"
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+          >
+            <CarouselContent className="-ml-1">
+              {tournaments.map((response) => (
+                <CarouselItem
+                  className="md:basis-1/2 lg:basis-1/3"
+                  key={response.id}
+                >
+                  <Card className="w-full h-full">
+                    <CardContent className="flex flex-col justify-between h-full">
+                      <div className="flex mt-4 gap-2 justify-between flex-row">
+                        <div>
+                          <CardTitle>{response.name}</CardTitle>
+                          <CardDescription>
+                            {response.description || "No description"}
+                          </CardDescription>{" "}
                         </div>
-                        <div className="mt-2">
-                          <TournamentDetail
-                            icon={<Users className="w-5 h-5 mr-2" />}
-                            title="Members"
-                            result={response.users_count}
+                        <Link
+                          to={`/tournaments/${response.id}`}
+                          className="items-center w-1/2"
+                        >
+                          <img
+                            src={response.image_url || undefined}
+                            alt={response.name}
+                            className="rounded-lg"
                           />
-                          <TournamentDetail
-                            icon={<DollarSign className="w-5 h-5 mr-2" />}
-                            title="Cash Prize"
-                            result={`$${response.cash_prize || 0}`}
-                          />
-                          <TournamentDetail
-                            icon={<BarChart className="w-5 h-5 mr-2" />}
-                            title="Status"
-                            result={response.status}
-                          />
-                          <TournamentDetail
-                            icon={<Gamepad className="w-5 h-5 mr-2" />}
-                            title="Game"
-                            result={response.game_name}
-                          />
-                        </div>
-                      </CardContent>
-                    </Card>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
-          </section>
+                        </Link>
+                      </div>
+                      <div className="mt-2">
+                        <TournamentDetail
+                          icon={<Users className="w-5 h-5 mr-2" />}
+                          title="Members"
+                          result={response.users_count}
+                        />
+                        <TournamentDetail
+                          icon={<DollarSign className="w-5 h-5 mr-2" />}
+                          title="Cash Prize"
+                          result={`$${response.cash_prize || 0}`}
+                        />
+                        <TournamentDetail
+                          icon={<BarChart className="w-5 h-5 mr-2" />}
+                          title="Status"
+                          result={response.status}
+                        />
+                        <TournamentDetail
+                          icon={<Gamepad className="w-5 h-5 mr-2" />}
+                          title="Game"
+                          result={response.game_name}
+                        />
+                      </div>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         ) : null}
-      </div>
+      </section>
     </div>
   );
 }
@@ -105,7 +112,7 @@ function TournamentDetail({
   result: string;
 }) {
   return (
-    <div className="flex justify-between flex-col lg:flex-row">
+    <div className="flex justify-between flex-row">
       <p className="flex font-medium text-normal items-center">
         {icon}
         {title}
