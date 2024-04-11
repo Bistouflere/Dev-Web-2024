39dded4ce1f42e303e836c283748b001e5546530
Simon Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2024-04-11 11:02:50 +0200

    added past tournaments to user info

--- frontend/src/routes/users.userId.tsx
+++ frontend/src/routes/users.userId.tsx
@@ -248,50 +248,53 @@ export default function UserProfile() {
                     </TableHeader>
                     <TableBody>
                       {tournaments
-                        ? tournaments.map((tournament) => (
-                            <TableRow key={tournament.id}>
-                              <TableCell className="w-[100px]">
-                                <Link to={`/tournaments/${tournament.id}`}>
-                                  <Avatar className="items-center w-10 h-10">
-                                    <AvatarImage
-                                      src={tournament.image_url || undefined}
-                                      alt="Avatar"
-                                      className="rounded-full"
-                                    />
-                                    <AvatarFallback>
-                                      {tournament.id}
-                                    </AvatarFallback>
-                                  </Avatar>
-                                </Link>
-                              </TableCell>
-                              <TableCell className="font-medium">
-                                <Link to={`/tournaments/${tournament.id}`}>
-                                  {tournament.name}
-                                </Link>
-                              </TableCell>
-                              <TableCell>{tournament.users_count}</TableCell>
-                              <TableCell>
-                                <Link to={`/games/${tournament.game_id}`}>
-                                  {tournament.game_name}
-                                </Link>
-                              </TableCell>
-                              <TableCell>{tournament.format}</TableCell>
-                              <TableCell>
-                                {tournament.visibility === "public" ? (
-                                  <Users className="h-5 w-5 text-green-500" />
-                                ) : (
-                                  <Users className="h-5 w-5 text-red-500" />
-                                )}
-                              </TableCell>
-                              <TableCell>
-                                ${tournament.cash_prize || 0}
-                              </TableCell>
+                        ? tournaments.map((tournament) =>
+                            tournament.status === "upcoming" ||
+                            tournament.status === "active" ? (
+                              <TableRow key={tournament.id}>
+                                <TableCell className="w-[100px]">
+                                  <Link to={`/tournaments/${tournament.id}`}>
+                                    <Avatar className="items-center w-10 h-10">
+                                      <AvatarImage
+                                        src={tournament.image_url || undefined}
+                                        alt="Avatar"
+                                        className="rounded-full"
+                                      />
+                                      <AvatarFallback>
+                                        {tournament.id}
+                                      </AvatarFallback>
+                                    </Avatar>
+                                  </Link>
+                                </TableCell>
+                                <TableCell className="font-medium">
+                                  <Link to={`/tournaments/${tournament.id}`}>
+                                    {tournament.name}
+                                  </Link>
+                                </TableCell>
+                                <TableCell>{tournament.users_count}</TableCell>
+                                <TableCell>
+                                  <Link to={`/games/${tournament.game_id}`}>
+                                    {tournament.game_name}
+                                  </Link>
+                                </TableCell>
+                                <TableCell>{tournament.format}</TableCell>
+                                <TableCell>
+                                  {tournament.visibility === "public" ? (
+                                    <Users className="h-5 w-5 text-green-500" />
+                                  ) : (
+                                    <Users className="h-5 w-5 text-red-500" />
+                                  )}
+                                </TableCell>
+                                <TableCell>
+                                  ${tournament.cash_prize || 0}
+                                </TableCell>
 
-                              <TableCell className="text-right">
-                                {tournament.tournament_role}
-                              </TableCell>
-                            </TableRow>
-                          ))
+                                <TableCell className="text-right">
+                                  {tournament.tournament_role}
+                                </TableCell>
+                              </TableRow>
+                            ) : null,
+                          )
                         : null}
                     </TableBody>
                   </Table>
@@ -303,7 +306,75 @@ export default function UserProfile() {
                 <CardHeader>
                   <CardTitle>{user.username}'s Past Tournaments</CardTitle>
                 </CardHeader>
-                <CardContent></CardContent>
+                <CardContent>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="w-[100px]">
+                          <span className="sr-only">Tournament Avatar</span>
+                        </TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Members</TableHead>
+                        <TableHead>Game</TableHead>
+                        <TableHead>Format</TableHead>
+                        <TableHead>Visibility</TableHead>
+                        <TableHead>Cash Prize</TableHead>
+                        <TableHead className="text-right">Role</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {tournaments
+                        ? tournaments.map((tournament) =>
+                            tournament.status === "completed" ||
+                            tournament.status === "cancelled" ? (
+                              <TableRow key={tournament.id}>
+                                <TableCell className="w-[100px]">
+                                  <Link to={`/tournaments/${tournament.id}`}>
+                                    <Avatar className="items-center w-10 h-10">
+                                      <AvatarImage
+                                        src={tournament.image_url || undefined}
+                                        alt="Avatar"
+                                        className="rounded-full"
+                                      />
+                                      <AvatarFallback>
+                                        {tournament.id}
+                                      </AvatarFallback>
+                                    </Avatar>
+                                  </Link>
+                                </TableCell>
+                                <TableCell className="font-medium">
+                                  <Link to={`/tournaments/${tournament.id}`}>
+                                    {tournament.name}
+                                  </Link>
+                                </TableCell>
+                                <TableCell>{tournament.users_count}</TableCell>
+                                <TableCell>
+                                  <Link to={`/games/${tournament.game_id}`}>
+                                    {tournament.game_name}
+                                  </Link>
+                                </TableCell>
+                                <TableCell>{tournament.format}</TableCell>
+                                <TableCell>
+                                  {tournament.visibility === "public" ? (
+                                    <Users className="h-5 w-5 text-green-500" />
+                                  ) : (
+                                    <Users className="h-5 w-5 text-red-500" />
+                                  )}
+                                </TableCell>
+                                <TableCell>
+                                  ${tournament.cash_prize || 0}
+                                </TableCell>
+
+                                <TableCell className="text-right">
+                                  {tournament.tournament_role}
+                                </TableCell>
+                              </TableRow>
+                            ) : null,
+                          )
+                        : null}
+                    </TableBody>
+                  </Table>
+                </CardContent>
               </Card>
             </div>
           </div>{" "}
