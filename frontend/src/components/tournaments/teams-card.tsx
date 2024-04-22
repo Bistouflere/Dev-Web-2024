9a09f982f1379846c606b8edad30310827d26006
guignome54 <guillaume.ladriere2@gmail.com>
2024-04-22 18:29:03 +0200

    tournamentPageUpdate

---
+++ frontend/src/components/tournaments/teams-card.tsx
@@ -0,0 +1,75 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import useWindowDimensions from "@/hooks/window-dimensions";
+import { Team, Tournament } from "@/types/apiResponses";
+import { Link } from "react-router-dom";
+
+export default function TournamentsTeamCard({
+  tournament,
+  teams,
+  setTab,
+}: {
+  tournament: Tournament;
+  teams: Team[];
+  setTab: (value: string) => void;
+}) {
+  const { width } = useWindowDimensions();
+  const breakpoint = width < 640 ? "sm" : width < 1280 ? "lg" : "xl";
+
+  const userDisplayCount = {
+    sm: 3,
+    lg: 5,
+    xl: 9,
+  };
+
+  const visibleUsers = teams.slice(0, userDisplayCount[breakpoint]);
+  const hiddenUsersCount = teams.length - visibleUsers.length;
+
+  return (
+    <div className="flex-auto">
+      <Card>
+        <CardHeader>
+          <CardTitle>{tournament.name}'s Teams</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-wrap gap-4">
+          {visibleUsers.map((tournament, index) => (
+            <TeamCard
+              key={tournament.id}
+              team={tournament}
+              className={`${index >= userDisplayCount[breakpoint] ? "hidden" : ""}`}
+            />
+          ))}
+          {hiddenUsersCount > 0 && (
+            <div
+              onClick={() => {
+                setTab("tournaments");
+              }}
+              className="flex h-20 w-full cursor-pointer items-center gap-2 rounded-lg border bg-secondary p-4 hover:border-primary lg:w-60"
+            >
+              <span className="text-lg font-semibold">
+                + {hiddenUsersCount} tournaments ...
+              </span>
+            </div>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+function TeamCard({ team, className }: { team: Team; className?: string }) {
+  return (
+    <Link
+      to={`/teams/${team.id}`}
+      className={`flex h-20 w-full items-center gap-4 rounded-lg border bg-secondary p-4 hover:border-primary lg:w-60 ${className}`}
+    >
+      <img
+        className="aspect-square h-10 w-10 rounded-md object-cover sm:h-12 sm:w-12"
+        src={team.image_url || undefined}
+        alt={team.name}
+      />
+      <div className="flex flex-col">
+        <span className="text-lg font-semibold">{team.name}</span>
+      </div>
+    </Link>
+  );
+}
--- frontend/src/routes/tournaments.tournamentId.tsx
+++ frontend/src/routes/tournaments.tournamentId.tsx
@@ -1,14 +1,16 @@
 import {
   tournamentQueryOptions,
+  tournamentTeamsQueryOptions,
   tournamentUsersQueryOptions,
 } from "@/api/tournaments";
 import TournamentMembersCard from "@/components/tournaments/members-card";
+import TournamentsTeamCard from "@/components/tournaments/teams-card";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@clerk/clerk-react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import dayjs from "dayjs";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function TournamentProfile() {
@@ -17,7 +19,11 @@ export default function TournamentProfile() {
   const { data: users } = useQuery(
     tournamentUsersQueryOptions(searchTournamentId || ""),
   );
-  // const [tab, setTab] = useState("overview");
+  const { data: team } = useQuery(
+    tournamentTeamsQueryOptions(searchTournamentId || ""),
+  );
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [tab, setTab] = useState("overview");
   const queryClient = useQueryClient();
   const { getToken } = useAuth();
   const invalidateQueries = useCallback(() => {
@@ -30,79 +36,83 @@ export default function TournamentProfile() {
   }, [queryClient]);
 
   return (
-    <>
-      <div className="container relative">
-        <div>
-          <div className="flex gap-7">
-            {data ? (
-              <img
-                className="aspect-square h-32 w-32 rounded-md object-cover"
-                alt={data.name}
-                src={data.image_url || undefined}
-                loading="lazy"
-              />
-            ) : null}
-            <div>
-              <p>prize : {data ? `${data.cash_prize || 0}` : "loading"}</p>
-              <p>status : {data ? data.status : "loading"}</p>
-              <p>format : {data ? data.format : "loading"}</p>
-              <p>
-                max team : {data ? data.max_team || "unlimited" : "loading"}
-              </p>
-              <p>max team size : {data ? data.max_team_size : "loading"}</p>
-              <p>min team size : {data ? data.min_team_size : "loading"}</p>
-              <p>
-                start date :{" "}
-                {data ? dayjs(data.start_date).format("DD/MM/YYYY") : "loading"}
-              </p>
-            </div>
-            <div className=" justify-end">
-              {data ? (
-                <Button
-                  className="flex justify-center sm:justify-end"
-                  onClick={async () => {
-                    const token = await getToken();
-                    await axios.post(
-                      `/api/tournaments/${data.id}/users`,
-                      {},
-                      {
-                        headers: { Authorization: `Bearer ${token}` },
-                      },
-                    );
-                    invalidateQueries();
-                  }}
-                >
-                  {" "}
-                  inscription tournoi{" "}
-                </Button>
-              ) : null}
-              {data ? (
-                <Button
-                  onClick={async () => {
-                    const token = await getToken();
-                    await axios.delete(`/api/tournaments/${data.id}/users`, {
-                      headers: { Authorization: `Bearer ${token}` },
-                    });
-                    invalidateQueries();
-                  }}
-                >
-                  Désinscription du tournoi
-                </Button>
-              ) : null}
-            </div>
+    <div className="container py-4">
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-7">
+          {data ? (
+            <img
+              className="aspect-square h-32 w-32 rounded-md object-cover"
+              alt={data.name}
+              src={data.image_url || undefined}
+            />
+          ) : null}
+          <div>
+            <p>prize : {data ? `${data.cash_prize || 0}` : "loading"}</p>
+            <p>status : {data ? data.status : "loading"}</p>
+            <p>format : {data ? data.format : "loading"}</p>
+            <p>max team : {data ? data.max_team || "unlimited" : "loading"}</p>
+            <p>max team size : {data ? data.max_team_size : "loading"}</p>
+            <p>min team size : {data ? data.min_team_size : "loading"}</p>
+            <p>
+              start date :{" "}
+              {data ? dayjs(data.start_date).format("DD/MM/YYYY") : "loading"}
+            </p>
           </div>
+          <div className=" justify-end">
+            {data ? (
+              <Button
+                className="flex justify-center sm:justify-end"
+                onClick={async () => {
+                  const token = await getToken();
+                  await axios.post(
+                    `/api/tournaments/${data.id}/users`,
+                    {},
+                    {
+                      headers: { Authorization: `Bearer ${token}` },
+                    },
+                  );
+                  invalidateQueries();
+                }}
+              >
+                {" "}
+                inscription tournoi{" "}
+              </Button>
+            ) : null}
+            {data ? (
+              <Button
+                onClick={async () => {
+                  const token = await getToken();
+                  await axios.delete(`/api/tournaments/${data.id}/users`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                  });
+                  invalidateQueries();
+                }}
+              >
+                Désinscription du tournoi
+              </Button>
+            ) : null}
+          </div>
+        </div>{" "}
+        <hr />
+        <div>
+          {data ? (
+            <TournamentMembersCard
+              tournament={data}
+              users={users || []}
+              //setTab={setTab}
+            ></TournamentMembersCard>
+          ) : null}
+        </div>
+        <div>
+          {data ? (
+            <TournamentsTeamCard
+              tournament={data}
+              teams={team || []}
+              setTab={setTab}
+            ></TournamentsTeamCard>
+          ) : null}
         </div>
       </div>
-      <hr />
-      <div>
-        {data ? (
-          <TournamentMembersCard
-            tournament={data}
-            users={users || []}
-            // setTab={setTab}
-          ></TournamentMembersCard>
-        ) : null}
-      </div>
-    </>
+    </div>
   );
 }
