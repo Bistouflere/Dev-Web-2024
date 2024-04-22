6cb477c5f3ceb1af4771bff5638e2cfbd0c4cb46
quentiinlvq <qleveque21@gmail.com>
2024-04-22 11:40:58 +0200

    add tournaments owner view

--- frontend/src/routes/dashboard/tournaments.tsx
+++ frontend/src/routes/dashboard/tournaments.tsx
@@ -1,7 +1,13 @@
+import { userTournamentsQueryOptions } from "@/api/users";
+import { DashboardTournamentList } from "@/components/dashboard/tournament-list";
+import { useAuth } from "@clerk/clerk-react";
+import { useQuery } from "@tanstack/react-query";
 import { ChevronRightIcon } from "lucide-react";
-import Balancer from "react-wrap-balancer";
 
 export default function TournamentPage() {
+  const { userId } = useAuth();
+  const { data: tournaments } = useQuery(userTournamentsQueryOptions(userId));
+
   return (
     <main className="relative py-6 lg:gap-10 lg:py-8">
       <div className="mx-auto w-full min-w-0">
@@ -16,9 +22,10 @@ export default function TournamentPage() {
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
             Your Tournaments
           </h1>
-          <p className="text-lg text-muted-foreground">
-            <Balancer>This is the protected settings page.</Balancer>
+          <p className="pb-2 text-xl text-muted-foreground">
+            Manage your tournaments and start competing with your friends!
           </p>
+          <DashboardTournamentList response={tournaments || []} />
         </div>
       </div>
     </main>
